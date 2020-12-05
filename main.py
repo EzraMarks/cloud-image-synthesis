972b58bc2f3d87eca838dbb8288c3535997cf0f1
Ezra Marks <ezrablankmarks@gmail.com>
2020-12-05 17:31:23 -0500

    Fix training for multiple epochs

--- main.py
+++ main.py
@@ -36,13 +36,14 @@ def train(real_images, masks, generator, discriminator):
     generator.optimizer.apply_gradients(zip(g_gradients, generator.trainable_variables))
 
     save_images(fake_images[0:1], "../results")
+    save_images(real_images[0:1], "../results/inputs")
 
 
 def main():
     # Define constants
     output_width_and_height = 256
-    batch_size = 4
-    num_epochs = 1
+    batch_size = 5
+    num_epochs = 20
 
     # Initialize preprocess and the models
     preprocess = Preprocess("../swimseg/images", "../swimseg/GTmaps", batch_size, dimension=output_width_and_height)
@@ -51,6 +52,7 @@ def main():
 
     # For each epoch train the models on each batch of inputs
     for epoch in range(num_epochs):
+        preprocess.inputs_processed = 0
         while True:
             clouds, masks = preprocess.get_data()
             if clouds is None or masks is None:
